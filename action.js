63fb4e5bc848dd5feca4e4d1f1f816afc78cd6e3
KICONCOPRECIOUSKELLEN <90697356+KICONCOPRECIOUSKELLEN@users.noreply.github.com>
2021-11-25 14:02:53 +0300

    Add files via upload

---
+++ action.js
@@ -0,0 +1,191 @@
+let text = 0;
+  //BUTTON CLICK ADDING TO CART
+  function addToCart() {
+    document.getElementById("collect").innerHTML =
+    text ++;
+  }
+  // BUTTON CLICK REMOVE FROM CART
+  function removeFromCart() {
+    document.getElementById("collect").innerHTML =
+    text --;
+  }
+  // CHANGE TEXT TO ORDERED, DISABLE WHEN CLICKED AND ACTIVATE OTHER BUTTON
+  function myFunction() {
+    document.getElementById("btn").innerHTML =
+    "Ordered";
+    //activate remove and disable ordered
+    document.getElementById("btn").disable =
+    true;
+    document.getElementById("dis").disable =
+    false;
+  }
+  function myFunction1() {
+    document.getElementById("btn1").innerHTML =
+    "Ordered";
+  }
+  function myFunction2() {
+    document.getElementById("btn2").innerHTML =
+    "Ordered";
+  }
+  function myFunction3() {
+    document.getElementById("btn3").innerHTML =
+    "Ordered";
+  }
+  function myFunction4() {
+    document.getElementById("btn4").innerHTML =
+    "Ordered";
+  }
+  function myFunction5() {
+    document.getElementById("btn5").innerHTML =
+    "Ordered";
+  }
+  function myFunction6() {
+    document.getElementById("btn6").innerHTML =
+    "Ordered";
+  }
+  function myFunction7() {
+    document.getElementById("btn7").innerHTML =
+    "Ordered";
+  }
+  function myFunction8() {
+    document.getElementById("btn8").innerHTML =
+    "Ordered";
+  }
+  function myFunction9() {
+    document.getElementById("btn9").innerHTML =
+    "Ordered";
+  }
+  function myFunction10() {
+    document.getElementById("btn10").innerHTML =
+    "Ordered";
+  }
+  function myFunction11() {
+    document.getElementById("btn11").innerHTML =
+    "Ordered";
+  }
+  function myFunction12() {
+    document.getElementById("btn12").innerHTML =
+    "Ordered";
+  }
+  function myFunction13() {
+    document.getElementById("btn13").innerHTML =
+    "Ordered";
+  }
+  function myFunction14() {
+    document.getElementById("btn14").innerHTML =
+    "Ordered";
+  }
+  function myFunction15() {
+    document.getElementById("btn15").innerHTML =
+    "Ordered";
+  }
+  function myFunction16() {
+    document.getElementById("btn16").innerHTML =
+    "Ordered";
+  }
+  function myFunction17() {
+    document.getElementById("btn17").innerHTML =
+    "Ordered";
+  }
+  function myFunction18() {
+    document.getElementById("btn18").innerHTML =
+    "Ordered";
+  }
+  function myFunction19() {
+    document.getElementById("btn19").innerHTML =
+    "Ordered";
+  }
+  function myFunction20() {
+    document.getElementById("btn20").innerHTML =
+    "Ordered";
+  }
+  //BUTTON CLICK REVERSE THE DISABLE AND ACTIVATE EFFECT CHANGE FROM REMOVE FROM CART TO REMOVED
+  function removed() {
+    document.getElementById("dis").innerHTML =
+    "Removed";
+    //activate ordered disable removed
+    document.getElementById("dis").disable =
+    true;
+    document.getElementById("btn").disable =
+    false;
+  }  
+  function removed1() {
+    document.getElementById("dis1").innerHTML =
+    "Removed";
+  }
+  function removed2() {
+    document.getElementById("dis2").innerHTML =
+    "Removed";
+  }
+  function removed3() {
+    document.getElementById("dis3").innerHTML =
+    "Removed";
+  }
+  function removed4() {
+    document.getElementById("dis4").innerHTML =
+    "Removed";
+  }
+  function removed5() {
+    document.getElementById("dis5").innerHTML =
+    "Removed";
+  }
+  function removed6() {
+    document.getElementById("dis6").innerHTML =
+    "Removed";
+  }
+  function removed7() {
+    document.getElementById("dis7").innerHTML =
+    "Removed";
+  }
+  function removed8() {
+    document.getElementById("dis8").innerHTML =
+    "Removed";
+  }
+  function removed9() {
+    document.getElementById("dis9").innerHTML =
+    "Removed";
+  }
+  function removed10() {
+    document.getElementById("dis10").innerHTML =
+    "Removed";
+  }
+  function removed11() {
+    document.getElementById("dis11").innerHTML =
+    "Removed";
+  }
+  function removed12() {
+    document.getElementById("dis12").innerHTML =
+    "Removed";
+  }
+  function removed13() {
+    document.getElementById("dis13").innerHTML =
+    "Removed";
+  }
+  function removed14() {
+    document.getElementById("dis14").innerHTML =
+    "Removed";
+  }
+  function removed15() {
+    document.getElementById("dis15").innerHTML =
+    "Removed";
+  }
+  function removed16() {
+    document.getElementById("dis16").innerHTML =
+    "Removed";
+  }
+  function removed17() {
+    document.getElementById("dis17").innerHTML =
+    "Removed";
+  }
+  function removed18() {
+    document.getElementById("dis18").innerHTML =
+    "Removed";
+  }
+  function removed19() {
+    document.getElementById("dis19").innerHTML =
+    "Removed";
+  }
+  function removed20() {
+    document.getElementById("dis20").innerHTML =
+    "Removed";
+  }
